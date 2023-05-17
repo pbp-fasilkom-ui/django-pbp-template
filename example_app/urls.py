a3cae5b89537dd46fb884aa04b2916cb4fffc7ab
TristanArdiansyah <nanda.tristan@ui.ac.id>
2023-05-17 22:47:39 +0700

    Revert "Revert "coba2 login""
    This reverts commit 8ddbcf386d07d8f8f0e774604ac091930fc169cf.

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from example_app.views import index
+from example_app.views import *
 
 app_name = 'example_app'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', login, name='login')
 ]
